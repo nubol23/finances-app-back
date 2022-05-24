c0b26fb5a705c9744be71d2bb455a15c517170a8
nubol23 <rafael.rvp98@gmail.com>
2022-05-24 19:21:30 -0400

    added test api view

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from apps.users.views import (
-    CustomTokenObtainPairView,
+    CustomTokenObtainPairView, TestAPIView,
 )
 
 app_name = "users"
@@ -10,4 +10,5 @@ urlpatterns = [
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("test/", TestAPIView.as_view())
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from apps.users import serializers
@@ -8,3 +12,10 @@ from apps.users import serializers
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = serializers.CustomTokenObtainPairSerializer
+
+
+class TestAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, *args, **kwargs):
+        return Response({"msg": "ok"}, status=status.HTTP_200_OK)
